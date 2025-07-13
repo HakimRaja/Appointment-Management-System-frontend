865582c7ab033769bbe4ebf1ba5d7f158298adf8
HakimRaja <hakim.raja756@gmail.com>
2025-07-14 00:21:26 +0500

    feat: setup routing and directory structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      he;;
+    {/* Header */}
+      <AppRoutes/>
+      {/* Footer */}
     </>
   )
 }
