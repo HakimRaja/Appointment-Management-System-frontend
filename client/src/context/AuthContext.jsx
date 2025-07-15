8a0a6254cd6adc46e253de6dd0f518939a6e5aea
HakimRaja <hakim.raja756@gmail.com>
2025-07-15 12:22:31 +0500

    complete auth context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,38 +1,37 @@
 import React,{createContext,useState,useEffect,useContext, use} from "react";
 import api from "../api/api";
 
-export const authContext = createContext();
+export const AuthContext = createContext();
 
-export const authContextProvider = ({children})=>{
-    const [medicalSpecializations,setMedicalSpecializations] = use([]);//for signup
+export const AuthContextProvider = ({children})=>{
+    const [medicalSpecializations,setMedicalSpecializations] = useState([]);//for signup
 
-    const [user,setUser] = use(null);
+    const [user,setUser] = useState(null);
 
-    const [signupSuccess, setSignupSuccess] = use(false);
-    const [signupError , setSignupError] = use(false);
+    const [signupSuccess, setSignupSuccess] = useState(false);
+    const [signupError , setSignupError] = useState(null);
     const [isSignupLoading , setIsSignupLoading] = useState(false);
-    const [signupInfo , setSignupInfo] = use({
+    const [signupInfo , setSignupInfo] = useState({
         name : '',
         email: '',
         password: '',
         phone: '',
         dob: '',
         role: 'admin',
-        specialization: '',
+        specialization_id: '',
         experience: '',
         history: ''
       });
-
-      const [loginSuccess, setLoginSuccess] = use(false);
-      const [loginError , setLoginError] = use(false);
+      const [loginSuccess, setLoginSuccess] = useState(false);
+      const [loginError , setLoginError] = useState(null);
       const [isLoginLoading , setIsLoginLoading] = useState(false);
-      const [loginInfo , setLoginInfo] = use({
+      const [loginInfo , setLoginInfo] = useState({
         email : '',
         password : ''
       })
-
+      // console.log(signupInfo);
       useEffect(() => { //setting up the user
-        const user = localStorage.getItem('User');
+        const user = JSON.parse(localStorage.getItem('User'));
         if (user) {
             setUser(user)
         }
@@ -48,11 +47,121 @@ export const authContextProvider = ({children})=>{
         })
         
       }, [])
-      
+
+      const loginUser = async(e) =>{
+        e.preventDefault();
+        setIsLoginLoading(true);
+        setLoginError(null);
+        setLoginSuccess(false);
+        try {
+          
+          const res = await api.post('/auth/login',loginInfo);
+          // if (!res?.message) {
+          //   throw error;
+          // }
+          setLoginSuccess(true)
+          localStorage.setItem('User',JSON.stringify(res.data
+            // userId : res?.userId,
+            // email : res?.email,
+            // name : res?.name,
+            // token : res?.token,
+            // role : res?.role
+          ));
+          setUser(res.data);
+          // alert(res?.message);
+
+          navigate('/');
+        } catch (error) {
+          // alert(error?.message || 'Signup Failed');
+          const errmsg = error?.response?.data?.message || error?.message || 'Login Failed';
+          setLoginError(errmsg);
+        }
+        finally{
+          setIsLoginLoading(false)
+        }
+      }
+
+      const signupUser = async(e)=>{
+        e.preventDefault();
+        console.log(signupInfo);
+        setIsSignupLoading(true);
+        setSignupError(null);
+        setSignupSuccess(false);
+        try {
+          
+          const res = await api.post('/auth/signup',signupInfo);
+          // if (!res?.message) {
+          //   throw error;
+          // }
+          setSignupSuccess(true)
+          localStorage.setItem('User',JSON.stringify(res?.data));
+          setUser(res.data);
+          // alert(res?.message);
+        } catch (error) {
+          // alert(error?.message || 'Signup Failed');
+          const errmsg = error?.response?.data?.message || error?.message || 'Signup Failed';
+          console.log(error);
+          setSignupError(errmsg);
+        }
+        finally{
+          setIsSignupLoading(false)
+        }
+      }
+
+      const updateSignupInfo = (info) =>{
+        setSignupInfo(info);
+      }
+      const updateLoginInfo = (info) =>{
+        setLoginInfo(info);
+      }
     
-    
-      return <authContext.Provider value={{user , medicalSpecializations , signupInfo , loginInfo,signupError,loginError,signupSuccess,loginSuccess,isSignupLoading,isLoginLoading}}>
+      const logoutUser = () =>{
+        localStorage.removeItem('User');
+        setUser(null);
+      }
+      const clearSignupState = () => {
+        setSignupSuccess(false);
+        setSignupError(null);
+        setSignupInfo({
+          name: '',
+          email: '',
+          password: '',
+          phone: '',
+          dob: '',
+          role: 'admin',
+          specialization_id: '',
+          experience: '',
+          history: ''
+        });
+      };
+      const clearLoginState = () => {
+        setLoginSuccess(false);
+        setLoginError(null);
+        setSignupInfo({
+          email: '',
+          password: ''
+        });
+      };
+
+      return <AuthContext.Provider value={{user ,
+       medicalSpecializations ,
+        signupInfo , 
+        loginInfo,
+        signupError,
+        loginError,
+        signupSuccess,
+        loginSuccess,
+        isSignupLoading,
+        isLoginLoading ,
+         signupUser ,
+          loginUser,
+          updateLoginInfo,
+          updateSignupInfo,
+          logoutUser,
+          clearSignupState,
+          clearLoginState
+          }}>
         {children}
-      </authContext.Provider>
+      </AuthContext.Provider>
 }
 
