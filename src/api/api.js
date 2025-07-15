fd903a7ce810e98e93ecee5637716ea1aa946612
HakimRaja <hakim.raja756@gmail.com>
2025-07-15 17:33:04 +0500

    fix: error in the api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const apiURL = import.meta.BACKEND_API_URL;
-
+const apiURL = import.meta.env.VITE_BACKEND_API_URL;
 const api = axios.create({
     baseURL: `${apiURL}`
 });
