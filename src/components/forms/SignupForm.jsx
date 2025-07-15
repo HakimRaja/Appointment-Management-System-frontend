8cd6476f2bfeb58ac99f1440683dd3e73823fd04
HakimRaja <hakim.raja756@gmail.com>
2025-07-15 21:07:48 +0500

    Signup form restored

---
+++ src/components/forms/SignupForm.jsx
@@ -0,0 +1,98 @@
+import React, { useContext, useState } from 'react'
+import Input from '../input/Input';
+import Button from '../buttons/Button';
+import DateInput from '../dateinputs/DateInput';
+import SpecializationSelect from '../scrollbars/SpecializationSelect';
+import { AuthContext} from '../../context/AuthContext';
+import {Link} from 'react-router-dom';
+import { SIGNUP_FIELDS } from '../../constants/signupFields';
+import Radio from '../radiobuttons/Radio';
+
+const SignupForm = () => {
+  const {medicalSpecializations , signupInfo ,signupError,signupSuccess,isSignupLoading , signupUser,updateSignupInfo } = useContext(AuthContext);
+  const handleChange = (e)=>{
+    if (e.target.multiple) {
+      const selectedSpecializations = Array.from(e.target.selectedOptions).map(option => option.value);
+      updateSignupInfo({...signupInfo , [e.target.name] : selectedSpecializations})
+    }
+    else{
+      updateSignupInfo({...signupInfo , [e.target.name] : e.target.value})
+    }
+  }
+
+  return (
+    <>
+    <form onSubmit={signupUser}>
+      <Input
+        {...SIGNUP_FIELDS.name}
+        value={signupInfo.name}
+        onChange={handleChange}
+        />
+     
+      <Input
+        {...SIGNUP_FIELDS.email}
+        value={signupInfo.email}
+        onChange={handleChange}
+        />
+      <Input
+        {...SIGNUP_FIELDS.password}
+        value={signupInfo.password}
+        onChange={handleChange}
+        />
+      <Input
+        {...SIGNUP_FIELDS.phone}
+        value={signupInfo.phone}
+        onChange={handleChange}
+        />
+    <DateInput
+    {...SIGNUP_FIELDS.dob}
+    onChange={(date,dateString) => updateSignupInfo({...signupInfo,dob:dateString})}
+    value={signupInfo.dob}
+    />
+
+      <Radio
+        {...SIGNUP_FIELDS.roleSelect}
+        array={SIGNUP_FIELDS.roles}
+        checked={signupInfo.role}
+        onChange={handleChange}
+      />
+      
+
+      {signupInfo.role === 'doctor' && (
+        <>
+      <DateInput
+    {...SIGNUP_FIELDS.experience}
+    onChange={(date,dateString) => updateSignupInfo({...signupInfo,experience:dateString})}
+    value={signupInfo.experience}
+    />
+    <SpecializationSelect
+    {...SIGNUP_FIELDS.specialization}
+    value={signupInfo.specialization}
+    onChange={(selectedOptions) =>{
+        selectedOptions = selectedOptions || [];
+        const ids = selectedOptions.map(opt => opt.value)
+        updateSignupInfo({...signupInfo,specialization : selectedOptions,specialization_id : ids})
+    }}
+    list={medicalSpecializations}
+    />
+      </>
+      
+      )}
+      {signupInfo.role === 'patient' && (
+        <Input
+        {...SIGNUP_FIELDS.history}
+        value={signupInfo.history}
+        onChange={handleChange}
+        />
+      )}
+
+      <Button type='submit' >{isSignupLoading ? 'Creating New User':'SignUp'}</Button>
+      <div className='text-red-400 mx-auto hover:underline hover:text-red-500 transition ease-in-out'><Link to='/login'>Go to login page</Link></div>
+    </form>
+    {signupSuccess && (<span className='bg-green-300'>Sign up Successfull</span>)}
+    {signupError && (<span className='bg-red-300'>{signupError}</span>)}
+    </>
+  )
+}
+
+export default SignupForm
