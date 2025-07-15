a181a7694309cbd187bf38d285609e864f85108e
HakimRaja <hakim.raja756@gmail.com>
2025-07-15 17:01:48 +0500

    add .env file and update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const apiURL = import.meta.BACKEND_API_URL;
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api'
+    baseURL: `${apiURL}`
 });
 
 export default api;
