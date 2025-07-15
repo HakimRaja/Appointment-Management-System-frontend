e047624d4eb7a05924661f9f3a945c34697bded9
HakimRaja <hakim.raja756@gmail.com>
2025-07-15 20:11:53 +0500

    add :  radio button component

---
+++ src/components/radiobuttons/Radio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Radio = ({label,required,name,onChange,array,labelClass,radioButtonsClass,type,checked}) => {
+  return (
+    <>
+          <label className={labelClass ? labelClass:'font-semibold'}>{label}{required && <span className='text-red-800'>*</span>}</label>
+          <div className={radioButtonsClass ? radioButtonsClass:'flex gap-4'}>
+              {array.map((e)=>(
+                <label key={e}>
+                  <input type={type} name={name} value={e} checked={checked === e} onChange={onChange} />{e}
+                </label>
+              ))}
+          </div>
+    </>
+  )
+}
+
+export default Radio
