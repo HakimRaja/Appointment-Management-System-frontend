66ac64fa7f4a93db906d0bdd3485544f9187528f
HakimRaja <hakim.raja756@gmail.com>
2025-07-16 14:31:38 +0500

    update: show user email instead of userID

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
   return (
     <div className='max-w-md mx-auto mt-10 p-4 border rounded shadow max-h-md bg-gray-300'>
-    <h1 className='text-red-700 font-extrabold'>Welcome {user.userId}</h1>
+    <h1 className='text-red-700 font-extrabold'>Welcome {user.email}</h1>
     <button onClick={logoutUser} className='bg-slate-600 rounded-2xl p-2'>Logout</button>
     </div>
   )
