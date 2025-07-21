847cd9aaad93d3180a13716cedc6d25c609c3407
HakimRaja <hakim.raja756@gmail.com>
2025-07-21 15:48:41 +0500

    update: Navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+  const location = useLocation();
   const {user , logoutUser} = useContext(AuthContext);
     return (
         <nav className='bg-gray-200 font-semibold flex flex-col sm:flex-row items-center justify-between p-3'>
@@ -13,8 +14,8 @@ const Navbar = () => {
             <div className='space-x-4 '>
                 {user && <button className='bg-blue-600 p-2 rounded-full hover:bg-blue-700 transition ease-in-out hover:scale-105 text-white' onClick={logoutUser}>Logout</button>}
                 {!user && <>
-                <Link className='bg-blue-600 p-2 rounded-full hover:bg-blue-700 transition ease-in-out hover:scale-105 text-white' to='/login'>Login</Link>
-                <Link className='bg-blue-600 p-2 rounded-full hover:bg-blue-700 transition ease-in-out hover:scale-105 text-white' to='/signup'>Signup</Link>
+                {location.pathname === '/signup' ? <Link className='bg-blue-600 p-3 rounded-full hover:bg-blue-700 transition ease-in-out hover:scale-105 text-white' to='/login'>Login</Link>:
+                <Link className='bg-blue-600 p-3 rounded-full hover:bg-blue-700 transition ease-in-out hover:scale-105 text-white' to='/signup'>Signup</Link>}
                 </>}
             </div>
             
