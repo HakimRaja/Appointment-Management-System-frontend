fd4da26ab784344c807b43e18125f9cd47e129c8
HakimRaja <hakim.raja756@gmail.com>
2025-07-29 09:26:40 +0500

    add: cross in the patient detail modal

--- src/components/modal/PatientDetailModal.jsx
+++ src/components/modal/PatientDetailModal.jsx
@@ -4,6 +4,7 @@ const PatientDetailModal = ({handleOnClose,patient}) => {
   return (
     <div className='inset-0 z-50 bg-black bg-opacity-75 fixed flex justify-center items-center'>
         <div className='bg-white p-6 space-y-2 font-semibold text-lg text-black text-left'>
+            <div className='text-right'><span className='p-1 bg-blue-500 rounded-full w-min hover:bg-blue-600 hover:scale-105 cursor-pointer transition ease-in-out' onClick={handleOnClose}>X</span></div>
             <h1>Name : {patient?.name}</h1>
             <h1>Email : {patient?.email}</h1>
             <h1>Phone : {patient?.phone_number}</h1>
