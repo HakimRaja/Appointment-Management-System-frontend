7dc7642fd959d4156f562b2445c3d5d5c6b1f80d
HakimRaja <hakim.raja756@gmail.com>
2025-07-29 11:39:51 +0500

    update: blur background when modal is open

--- src/components/modal/PatientDetailModal.jsx
+++ src/components/modal/PatientDetailModal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PatientDetailModal = ({handleOnClose,patient}) => {
   return (
-    <div className='inset-0 z-50 bg-black bg-opacity-75 fixed flex justify-center items-center'>
+    <div className='inset-0 z-50 bg-black backdrop-blur-sm bg-opacity-75 fixed flex justify-center items-center'>
         <div className='bg-white p-6 space-y-2 font-semibold text-lg text-black text-left'>
             <div className='text-right'><span className='p-1 bg-blue-500 rounded-full hover:bg-blue-600 hover:scale-105 cursor-pointer transition ease-in-out' onClick={handleOnClose}>X</span></div>
             <h1>Name : {patient?.name}</h1>
